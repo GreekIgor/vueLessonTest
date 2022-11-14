9b01ab5294e7f484b77984b5745af010b7d2b355
GreekIgor <40692365+GreekIgor@users.noreply.github.com>
2022-11-14 15:50:59 +0600

    добавлено недостающее звено

--- src/components/request/RequestTable.vue
+++ src/components/request/RequestTable.vue
@@ -17,8 +17,8 @@
             <td>{{idx +1 }}</td>
             <td>{{r.fio }}</td>
             <td>{{r.phone }}</td>
-            <td>{{r.amount }}</td>
-            <td>{{r.status }}</td>
+            <td>{{currency(r.amount) }}</td>
+            <td><AppStatus :type="r.status"/></td>
             <td>
                 <router-link v-slot="{navigate}" custom :to="{name: 'Request', params : {id: r.id}}">
                    <button class="btn primary" @click="navigate">Открыть</button>
@@ -33,9 +33,16 @@
 </template>
 
 <script>
+    import {currency} from '../../utils/curency'
+    import AppStatus from '../ui/AppStatus'
+
     export default {
         name: "RequestTable",
-        props: ['request']
+        components: {AppStatus},
+        props: ['request'],
+        setup() {
+            return {currency}
+        }
     }
 </script>
 
